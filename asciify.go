d0046ee8b95baa3f519b96fd31e6ed4dcba6b1d8
Nicholas <nicholas@spunow.co.uk>
2025-05-25 16:06:48 +0100

    Resize function added, and stricter typing to help me learn

--- asciify.go
+++ asciify.go
@@ -7,36 +7,66 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
+	"math"
 	"os"
+
+	"golang.org/x/image/draw"
 )
+
+func resize(img image.Image, width int) image.Image {
+	//Variable Declarations
+	var aspectRatio float64
+	var height int
+	var resized_image draw.Image
+
+	//Calculate new image height based on width
+	aspectRatio = (float64)(img.Bounds().Max.Y) / (float64)(img.Bounds().Max.X)
+	height = int(math.Round(float64(width) * aspectRatio))
+
+	//Resize image
+
+	resized_image = image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.NearestNeighbor.Scale(resized_image, resized_image.Bounds(), img, img.Bounds(), draw.Over, nil)
+
+	return resized_image
+
+}
 
 func main() {
 
+	//Variable Declarations
+	var imageFile *os.File
+	var err error
+	var img image.Image
+	var cd_map = [5]string{"*", "%", "D", "J", "@"}
+
 	//Load image
 
-	imageFile, err := os.Open("/home/nicholas/Pictures/hq_cat.png") // := is shorthand for regular variable declaration
+	imageFile, err = os.Open("./testimages/hq_cat.png") // := is shorthand for regular variable declaration
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer imageFile.Close() // defer runs when main returns
 
 	// Decode PNG
-	image, _, err := image.Decode(imageFile)
+	img, _, err = image.Decode(imageFile)
 	if err != nil {
 		log.Fatal(err)
+
 	}
 
-	levels := []string{"*", "%", "D", "J", "@"}
+	img = resize(img, 20)
 
-	for y := image.Bounds().Min.Y; y < image.Bounds().Max.Y; y++ {
-		for x := image.Bounds().Min.X; x < image.Bounds().Max.X; x++ {
-			c := color.GrayModel.Convert(image.At(x, y)).(color.Gray)
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			c := color.GrayModel.Convert(img.At(x, y)).(color.Gray) // Converts image to greyscale for intensity map and then gets grey color component
 			level := c.Y / 51
 			if level == 5 {
 				level--
 			}
-			fmt.Print(levels[level])
+			fmt.Print(cd_map[level])
 		}
+		fmt.Print("\n")
 	}
 
 }
--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module asciify
 
 go 1.24.3
 
+require golang.org/x/image v0.27.0
