3608a8b754278ba88e0b1ca02eed2d562e275abc
Nicholas <nicholas@spunow.co.uk>
2025-05-25 14:20:20 +0100

    Allows for other format of image

--- asciify.go
+++ asciify.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/color"
-	"image/png"
+	_ "image/jpeg"
+	_ "image/png"
 	"log"
 	"os"
 )
@@ -12,30 +14,29 @@ func main() {
 
 	//Load image
 
-	imageFile, err := os.Open("/home/nicholas/Pictures/sunlight.png") // := is shorthand for regular variable declaration
+	imageFile, err := os.Open("/home/nicholas/Pictures/hq_cat.png") // := is shorthand for regular variable declaration
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer imageFile.Close() // defer runs when main returns
 
 	// Decode PNG
-	image, err := png.Decode(imageFile)
+	image, _, err := image.Decode(imageFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	levels := []string{".", ",", "$", "Y", "+", "P", "*", "%", "D", "J", "@"}
+	levels := []string{"*", "%", "D", "J", "@"}
 
 	for y := image.Bounds().Min.Y; y < image.Bounds().Max.Y; y++ {
 		for x := image.Bounds().Min.X; x < image.Bounds().Max.X; x++ {
 			c := color.GrayModel.Convert(image.At(x, y)).(color.Gray)
-			level := c.Y / 23
-			if level == 11 {
+			level := c.Y / 51
+			if level == 5 {
 				level--
 			}
 			fmt.Print(levels[level])
 		}
-		fmt.Println("\n")
 	}
 
 }
