d10389b00f78dac68eba9e411caa4fc3557898e0
Nicholas <nicholas@spunow.co.uk>
2025-05-26 12:40:08 +0100

    Added colour !!!

--- cmd/root.go
+++ cmd/root.go
@@ -17,12 +17,18 @@ import (
 	"golang.org/x/image/draw"
 )
 
+func rgbToANSI(r, g, b uint32) string {
+	return fmt.Sprintf("\033[38;2;%d;%d;%dm", r, g, b)
+}
+
+const colorReset = "\033[0m"
+
 // resize function resizes an image to a specified width while maintaining aspect ratio
 func resize(img image.Image, width int) image.Image {
 	//Variable Declarations
 	var aspectRatio float64
 	var height int
-	var resized_image draw.Image
+	var resizedImage draw.Image
 
 	//Calculate new image height based on width
 	aspectRatio = (float64)(img.Bounds().Max.Y) / (float64)(img.Bounds().Max.X)
@@ -30,10 +36,10 @@ func resize(img image.Image, width int) image.Image {
 
 	//Resize image
 
-	resized_image = image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.NearestNeighbor.Scale(resized_image, resized_image.Bounds(), img, img.Bounds(), draw.Over, nil)
+	resizedImage = image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.NearestNeighbor.Scale(resizedImage, resizedImage.Bounds(), img, img.Bounds(), draw.Over, nil)
 
-	return resized_image
+	return resizedImage
 
 }
 
@@ -62,7 +68,8 @@ var rootCmd = &cobra.Command{
 	Long:  `Asciify is a command-line tool that converts .png .jpeg or .gif into ASCII art.`,
 	Args:  validateArguments,
 	Run: func(cmd *cobra.Command, args []string) {
-		widthFlag, _ := cmd.Flags().GetInt("width") // Get the width from the command line flag
+		widthFlag, _ := cmd.Flags().GetInt("width")  // Get the width from the command line flag
+		colorFlag, _ := cmd.Flags().GetBool("color") // Get the color flag from the command line flag
 		//Variable Declarations
 		var filepath string = args[0] // Get the file path from the command line argument
 		var imageFile *os.File
@@ -104,7 +111,18 @@ var rootCmd = &cobra.Command{
 				if level > len(cd_map) {
 					level--
 				}
-				fmt.Print(cd_map[level])
+				if colorFlag {
+					// Get RBG values of the pixel as 8 bit colors
+					var r, g, b uint32
+					r, g, b, _ = img.At(x, y).RGBA()
+					r = r >> 8
+					g = g >> 8
+					b = b >> 8
+					fmt.Print(rgbToANSI(r, g, b) + cd_map[level] + colorReset) // Print the ASCII character with ANSI color codes
+				} else {
+					fmt.Print(cd_map[level]) // Print the ASCII character without color
+				}
+
 			}
 			fmt.Print("\n")
 		}
@@ -122,4 +140,5 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().IntP("width", "w", 0, "Custom width of the output ASCII art in characters")
+	rootCmd.Flags().BoolP("color", "c", false, "Enable color output for the ASCII art using ANSI colours")
 }
